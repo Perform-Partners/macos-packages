c143344085b4b31bc3dc8e855f82b920ec673aee
John Summers <john.summers@perform-partners.com>
2024-05-22 15:49:54 +0100

    removed files

--- InstallerSetup.app/Contents/Resources/launchRemoteAccessUserHandler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#  launchAssistUserHandler.sh
-#  ZohoAssistAgent
-#
-#  Created by Boopathy P on 29/12/16.
-#  Copyright © 2016 Boopathy P. All rights reserved.
-
-exec /Library/Application\ Support/MERemoteAccess/AssistUserHandler "$USER" -productId 2
